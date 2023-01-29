a04d5b5da78660a320647e1bf3b40c7bc07561e2
0xpaluco <hello@0xpalu.co>
2023-01-28 23:50:42 -0400

    app props with layouts

---
+++ lib/types/index.d.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse, NextPage } from 'next'
+import { AppInitialProps, AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react'
+
+type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode
+    auth: boolean
+}
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+    //session: Session
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,50 @@
-import '@views/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@src/styles/globals.css'
+import { AppPropsWithLayout } from '@lib/types'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return (
+    <div className='h-full'>
+      {Component.auth ? (
+        <Auth>
+          {getLayout(<Component {...pageProps} />)}
+        </Auth>
+      ) : (
+        getLayout(<Component {...pageProps} />)
+      )}
+    </div>
+  )
+
+
+}
+
+interface AuthProps {
+  children: any
+}
+function Auth({ children }: AuthProps) {
+
+  // const { status } = useSession({ required: true })
+  // const isLoading = status === "loading";
+  // const isAuthenticated = status === "authenticated";
+
+  // // useEffect(() => {
+  // //   console.log(`isAuthenticated: ${isAuthenticated}`);
+  // //   console.log(`isLoading: ${isLoading}`);
+  // // },[isLoading, isAuthenticated])
+
+  // if (isLoading) {
+  //   return <div>Authenticating...</div>
+  // }
+
+  // if (isAuthenticated) {
+  //   return children;
+  // }
+
+  // // Session is being fetched, or no user.
+  // // If no user, useEffect() will redirect.
+  // return <AuthView />
+
+  return children;
 }

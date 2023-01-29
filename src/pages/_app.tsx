5b1a833e3fc11631890b9f63ad675bd8a3fd38b8
0xpaluco <hello@0xpalu.co>
2023-01-28 21:39:08 -0400

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@views/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}

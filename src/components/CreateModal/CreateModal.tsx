c64b710dcefd06a8ebfefa90c5911bb695ea0f80
0xpaluco <hello@0xpalu.co>
2023-03-06 02:18:07 -0400

    ipfs uoload for collection image data

--- src/components/CreateModal/CreateModal.tsx
+++ src/components/CreateModal/CreateModal.tsx
@@ -10,6 +10,7 @@ import { useRouter } from "next/router";
 import { kebabCase } from "lodash";
 
 import toast from "react-hot-toast";
+import IPFSUpload from "../IPFSUpload";
 
 type Collections = Database["public"]["Tables"]["collections"]["Row"];
 
@@ -130,49 +131,18 @@ export default function CreateModal({ open, setOpen }: CreateModalProps) {
                     </div>
                     <div className="grid w-full grid-cols-1 items-start gap-y-8 gap-x-6 sm:grid-cols-12 lg:gap-x-8">
                       <div className="sm:col-span-4 lg:col-span-5">
-                        <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg mt-6">
-                          <div>
-                            <label className="block text-sm font-medium leading-6 text-gray-900">
-                              Thumbnail
-                            </label>
-                            <div className="mt-2 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                              <div className="space-y-5 text-center">
-                                <svg
-                                  className="mx-auto h-12 w-12 text-gray-400"
-                                  stroke="currentColor"
-                                  fill="none"
-                                  viewBox="0 0 48 48"
-                                  aria-hidden="true"
-                                >
-                                  <path
-                                    d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                  />
-                                </svg>
-                                <div className="flex text-sm text-gray-600">
-                                  <label
-                                    htmlFor="file-upload"
-                                    className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                                  >
-                                    <span>Upload a file</span>
-                                    <input
-                                      id="file-upload"
-                                      name="file-upload"
-                                      type="file"
-                                      className="sr-only"
-                                    />
-                                  </label>
-                                  <p className="pl-1">or drag and drop</p>
-                                </div>
-                                <p className="text-xs text-gray-500">
-                                  PNG, JPG, GIF up to 10MB
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                      
+                      <IPFSUpload
+                          label={"Thumbnail"}
+                          name="thumbnail"
+                          fileType="image/*"
+                          ipfsUrl={thumbnail_url}
+                          onUpload={(url) => {
+                            setThumbnail_url(url);
+                          }}
+                        />
+
+                     
                       </div>
 
                       <div className="sm:col-span-8 lg:col-span-7">
